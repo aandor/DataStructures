f1a5be7dc515e2924a9f790773d652c2cfaf79b2
aandor <lucas.hernani.2001@gmail.com>
2023-09-14 10:40:17 -0300

    New methods added

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -9,6 +9,28 @@ class PriorityQueue:
         """Return a string representation of the priority queue."""
         return str([f"{val} (Priority: {priority})" for priority, val in sorted(self.queue)])
 
+    def clear(self):
+        """Clear all elements from the priority queue."""
+        self.queue = []
+
+    def contain(self, val):
+        """Check if a value exists in the priority queue.
+        
+        :param val: The value to be checked.
+        :return: True if the value exists, False otherwise.
+        """
+        for i in range(len(self.queue)):
+            if val == self.queue[i][1]:
+                return True
+        return False
+
+    def is_empty(self):
+        """Check if the priority queue is empty.
+        
+        :return: True if empty, False otherwise.
+        """
+        return not self.queue
+
     def push(self, val, priority):
         """Add an element to the queue with a given priority."""
         self.queue.append((priority, val))
@@ -23,7 +45,29 @@ class PriorityQueue:
         self.queue.pop()
         self._heapify_down(0)
         return val
-
+    
+    def size(self):
+        """Return the number of elements in the priority queue.
+        
+        :return: Integer representing the size of the queue.
+        """
+        return len(self.queue)
+    
+    def update_priority(self, val, new_priority):
+        """Update the priority of an existing element in the priority queue.
+        
+        :param val: The value whose priority needs to be updated.
+        :param new_priority: The new priority value.
+        :return: True if updated, False otherwise.
+        """
+        for i, (priority, value) in enumerate(self.queue):
+            if value == val:
+                self.queue[i] = (new_priority , val)
+                self._heapify_up(i)
+                self._heapify_down(i)
+                return True
+        return False
+                
     def _heapify_up(self, index):
         """Heapify upwards starting from a given index."""
         parent = (index - 1) // 2
@@ -46,11 +90,22 @@ class PriorityQueue:
 
 # Test cases
 pq = PriorityQueue()
+assert pq.is_empty() == True
 pq.push("medium", 1)
+assert pq.is_empty() == False
+assert pq.size() == 1
+assert pq.contain("medium") == True
+assert pq.contain("high") == False
 pq.push("high", 0)
 pq.push("low", 2)
+assert pq.size() == 3
 print("Priority Queue:", pq)
 assert pq.pop() == "high"
 assert pq.pop() == "medium"
 assert pq.pop() == "low"
 assert pq.pop() == None
+assert pq.is_empty() == True
+pq.clear()
+assert pq.is_empty() == True
+
+print("All test cases passed!")
